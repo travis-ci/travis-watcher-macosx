f2932d2326949b562f6be8d125651bc4b4e8b9d5
phoet <phoetmail@googlemail.com>
2011-08-19 14:30:15 +0200

    make it run

--- ApplicationController.rb
+++ ApplicationController.rb
@@ -1,7 +1,5 @@
 class ApplicationController
   def applicationDidFinishLaunching(notification)
-    GrowlApplicationBridge.growlDelegate = self
-
     @pusher = PTPusher.alloc.initWithKey(TravisToolbar::PUSHER_API_KEY, channel:TravisToolbar::PUSHER_CHANNEL_NAME)
 
     @pusher.addEventListener(TravisToolbar::BUILD_STARTED,  target:self, selector:"handle_event:")
@@ -11,12 +9,6 @@ class ApplicationController
   end
 
   def handle_event(event)
-    GrowlApplicationBridge.notifyWithTitle(event.name,
-      description:event.data["build"]["repository"]["name"],
-      notificationName:"Build Information",
-      iconData:NSImage.imageNamed("travis_logo.png").TIFFRepresentation,
-      priority:0,
-      isSticky:false,
-      clickContext:nil)
+    puts event.name
   end
 end
