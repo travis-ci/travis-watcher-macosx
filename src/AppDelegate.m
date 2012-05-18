140c0dbe8d17f579411576ecd50544aa71b2fa04
Henrik Hodne <dvyjones@dvyjones.com>
2012-05-18 02:07:10 +0200

    Add Growl 1.2.3 for Snow Leopard

--- src/AppDelegate.m
+++ src/AppDelegate.m
@@ -20,11 +20,23 @@
 @synthesize window = _window;
 @synthesize pusher = _pusher;
 @synthesize statusMenu = _statusMenu;
+@synthesize preferencesPanel = _preferencesPanel;
 @synthesize statusItem = _statusItem;
 @synthesize statusImage = _statusImage;
 @synthesize statusHighlightImage = _statusHighlightImage;
 
 - (void)applicationDidFinishLaunching:(NSNotification *)notification {
+  NSBundle *mainBundle = [NSBundle mainBundle];
+  NSString *path = [[mainBundle privateFrameworksPath] stringByAppendingPathComponent:@"Growl"];
+  if (NSAppKitVersionNumber >= 1038)
+    path = [path stringByAppendingPathComponent:@"1.3"];
+  else
+    path = [path stringByAppendingPathComponent:@"1.2.3"];
+
+  path = [path stringByAppendingPathComponent:@"Growl.framework"];
+  NSBundle *growlFramework = [NSBundle bundleWithPath:path];
+  [growlFramework load];
+
   self.pusher = [[PusherConnection alloc] init];
   
   self.statusItem = [[NSStatusBar systemStatusBar] statusItemWithLength:NSSquareStatusItemLength];
--- src/PusherConnection.m
+++ src/PusherConnection.m
@@ -46,24 +46,30 @@
 
 - (void)handleStarted:(PTPusherEvent *)event {
   TravisEventData *eventData = [[TravisEventData alloc] initWithEventData:event.data];
-  [GrowlApplicationBridge notifyWithTitle:eventData.name
-                              description:@"Starting build!"
-                         notificationName:@"Build Information"
-                                 iconData:[[NSImage imageNamed:@"travis_logo.png"] TIFFRepresentation]
-                                 priority:0
-                                 isSticky:NO
-                             clickContext:nil];
+  Class GAB = NSClassFromString(@"GrowlApplicationBridge");
+  if ([GAB respondsToSelector:@selector(notifyWithTitle:description:notificationName:iconData:priority:isSticky:clickContext:identifier:)])
+    [GAB notifyWithTitle:eventData.name
+             description:@"Starting build!"
+        notificationName:@"Build Information"
+                iconData:[[NSImage imageNamed:@"travis_logo.png"] TIFFRepresentation]
+                priority:0
+                isSticky:NO
+            clickContext:nil
+              identifier:eventData.name];
 }
 
 - (void)handleFinished:(PTPusherEvent *)event {
   TravisEventData *eventData = [[TravisEventData alloc] initWithEventData:event.data];
-  [GrowlApplicationBridge notifyWithTitle:eventData.name
-                              description:[NSString stringWithFormat:@"Finished build with status: %@", eventData.status]
-                         notificationName:@"Build Information"
-                                 iconData:[[NSImage imageNamed:@"travis_logo.png"] TIFFRepresentation]
-                                 priority:0
-                                 isSticky:NO
-                             clickContext:nil];
+  Class GAB = NSClassFromString(@"GrowlApplicationBridge");
+  if ([GAB respondsToSelector:@selector(notifyWithTitle:description:notificationName:iconData:priority:isSticky:clickContext:identifier:)])
+    [GAB notifyWithTitle:eventData.name
+             description:[NSString stringWithFormat:@"Finished build with status: %@", eventData.status]
+        notificationName:@"Build Information"
+                iconData:[[NSImage imageNamed:@"travis_logo.png"] TIFFRepresentation]
+                priority:0
+                isSticky:NO
+            clickContext:nil
+              identifier:eventData.name];
 }
 
 @end
